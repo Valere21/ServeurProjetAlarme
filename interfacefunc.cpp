3086743a49a70f3c7e23de22295c6671ae82b4cf
Valere21 <valere.gh@hotmail.fr>
2020-06-25 01:02:05 +0200

    ajout txt sur voyant

--- interfacefunc.cpp
+++ interfacefunc.cpp
@@ -149,10 +149,10 @@ void Interface::getSensorState(QByteArray msg)
         qDebug() << "lightSoundItem not found";
         return ;
     }
-   /* if (labelArchive == nullptr){
+    if (labelArchive == nullptr){
         qDebug() << "labelArchiveItem not found";
         return ;
-    }*/
+    }
     qDebug() << "labelArhive Found !";
     labelArchive->setProperty("varSensor", "val Change");
 
